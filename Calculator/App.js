8b9e5f665120c9c93a5fdad10a76f9ded7cad932
Diogobsm <diogobsmachado@gmail.com>
2024-08-01 14:46:36 -0300

    cores na parte superior

--- Calculator/App.js
+++ Calculator/App.js
@@ -9,7 +9,6 @@ export default function App() {
         <View style={[styles.colorBox, { backgroundColor: 'red', flex: 0.7 }]} />
         <View style={[styles.colorBox, { backgroundColor: 'yellow', flex: 1 }]} />
         <View style={[styles.colorBox, { backgroundColor: 'green', flex: 1 }]} />
-        <View style={[styles.colorBox, { backgroundColor: 'blue', flex: 0.7 }]} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -20,14 +19,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   colorColumn: {
     flexDirection: 'column',
     width: '100%',
-    height: '100%',
-    justifyContent: 'center',
+    height: '50%', 
+    justifyContent: 'flex-start', 
   },
   colorBox: {
     width: '100%',
